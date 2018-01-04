1f4320265cac831663f4e693691e27b718206d6f
pan.shu <pan.shu@mobvista.com>
2018-01-04 17:55:21 +0800

    + 初步完成了Encrypter 加密/解密工作。 暂时支持AES的加密方式。

---
+++ src/Super/Api/Encryption/EncrypterApi.php
@@ -0,0 +1,29 @@
+<?php
+namespace Super\Api\Encryption;
+
+/**
+ * 加密的方式
+ * User: phil.shu
+ * Date: 2018/1/4
+ * Time: 下午4:06
+ */
+interface Encrypter
+{
+
+    /**
+     * 给值进行加密操作
+     * @param $value
+     * @param bool $serialize
+     * @return mixed
+     */
+    public function encrypt($value , $serialize = true);
+
+
+    /**
+     * 解密的操作
+     * @param $payload
+     * @param bool $unserialize
+     * @return mixed
+     */
+    public function decrypt($payload , $unserialize = true);
+}
