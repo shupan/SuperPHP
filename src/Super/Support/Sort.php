5a15c64afa1f3156d5b43c6ad2c6ab371dc91d68
pan.shu <835384484@qq.com>
2018-01-15 23:51:07 +0800

    + 支持排序的扩展

---
+++ src/Super/Support/Sort.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * User: phil.shu
+ * Date: 2018/1/15
+ * Time: 下午11:33
+ */
+
+namespace Super\Support;
+
+
+/**
+ * 排序算法, 主要是满足不同场景的接口的使用
+ *
+ * Class Sort
+ * @package Super\Support
+ */
+class Sort
+{
+
+    /**
+     * 冒泡数组排序
+     * @param array $arr
+     * @return array
+     */
+    public function bubbleSort(array $arr)
+    {
+
+        return $arr;
+    }
+
+    /**
+     * 插入排序的实现
+     * @param array $arr
+     * @return array
+     */
+    public function insertSort(array $arr)
+    {
+        return $arr;
+    }
+
+    /**
+     * 快速排序的实现
+     * @param array $arr
+     * @return array
+     */
+    public function fastSort(array $arr)
+    {
+
+        return $arr;
+    }
+
+}
