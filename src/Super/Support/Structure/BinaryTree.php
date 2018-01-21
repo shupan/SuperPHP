32eba31e72cbe24d0c50a2297e471788afc41d4c
pan.shu <835384484@qq.com>
2018-01-22 00:26:19 +0800

    + 树的开始构建

--- src/Super/Support/Structure/BinaryTree.php
+++ src/Super/Support/Structure/BinaryTree.php
@@ -8,11 +8,45 @@
 namespace Super\Support\Structure;
 
 
-class BinaryTree implements  Tree
+/**
+ * 构建一棵二叉树
+ * 描述:
+ * 1. 构建二叉树的根节点
+ * 2.
+ *
+ * Class BinaryTree
+ * @package Super\Support\Structure
+ */
+class BinaryTree implements Tree
 {
 
+    protected $arr = [];
+
     /**
-     * 创建数
+     * 构建二叉树
+     * BinaryTree constructor.
+     * @param array $arr
+     */
+    public function __construct(Array $arr)
+    {
+
+        $this->arr = $arr;
+    }
+
+    /**
+     * 初始化二叉树
+     */
+    protected function initTree()
+    {
+
+        foreach ($this->arr as $key => $val) {
+
+            
+        }
+    }
+
+    /**
+     * 增加二叉树节点,
      * @return mixed
      */
     public function create()
@@ -58,5 +92,60 @@ class BinaryTree implements  Tree
     public function deleteAll()
     {
         // TODO: Implement deleteAll() method.
+    }
+}
+
+/**
+ * 树节点的构建
+ * Class TreeNode
+ * @package Super\Support\Structure
+ */
+class TreeNode
+{
+    /**
+     * 跟节点
+     * @var TreeNode
+     */
+    protected $root = null;
+
+    /**
+     * 左节点
+     * @var TreeNode
+     */
+    protected $left = null;
+
+    /**
+     * 右节点
+     * @var TreeNode
+     */
+    protected $right = null;
+
+    /**
+     * 节点值
+     * @var Node
+     */
+    protected $node = null;
+
+    /**
+     * 构建左右节点
+     * TreeNode constructor.
+     * @param $root
+     * @param $left
+     * @param $right
+     * @param $node
+     */
+    public function __construct($root, $left = null, $right = null, $node = null)
+    {
+        if ($node != null) {
+            $this->node = $node;
+        }
+        $this->root = $root;
+        if ($this->root->left != null) {
+            $this->root->left = $left;
+        }
+
+        if ($this->root->right != null) {
+            $this->root->right = $right;
+        }
     }
 }
